7c7a644777b55820fca0a09fc2d1021c50a8c218
Lucas Taveira <lucas@Mac-mini-de-Lucas.local>
2022-10-27 16:08:12 -0300

    last updates

--- ecomerce-front/ecomerce-front/src/App.jsx
+++ ecomerce-front/ecomerce-front/src/App.jsx
@@ -2,13 +2,17 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
 
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 
 function App() {
 
   const [products, setProduct] = useState([])
   const [shoppingcart, setShoppingCart] = useState([])
+
+  const initialValue = 0;
+  const sumWithInitial = shoppingcart.reduce(
+    (previousValue, product) => previousValue + Number(product.value),
+    initialValue
+  );
 
   useEffect(()=>{
     async function getProducts(){
@@ -28,18 +32,25 @@ function App() {
       <h2>listagem de produtos</h2>
       <ol>
       {products.map((product)=>(
-        <li key={product.id}>
-          <img src={product.picture} alt="" />
+        <li key={product.id} className="product">
+          <img src={product.picture} alt="" className='images'/>
+          <p>
+          {product.name}
+          </p>
+          <p>
+          R$ {product.value}
+          </p>
           <button onClick={()=>addProductShoppingCart(product)}>Adicionar ao carrinho</button>
         </li>
       ))}   
       </ol>
       <hr />
-      <h3>Carrinho</h3>
+      <h3>Carrinho R$ {sumWithInitial.toFixed(2)}</h3>
       <ol>
       {shoppingcart.map((product)=>(
         <li key={product.id}>
           {product.name}
+          {}
         </li>
       ))}
       </ol>
